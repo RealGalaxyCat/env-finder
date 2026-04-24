af893c6bbc0cf4a7ec987bb507ffcb38292bc5a0
realgalaxycat <111510472+RealGalaxyCat@users.noreply.github.com>
2026-04-24 09:36:31 +0200

    refactor(entrypoint): modify entrypoint file so it starts scraper and API

--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
+from fastapi import FastAPI
+import uvicorn
+from threading import Thread
+
+from api.api import app
 from scraper import Scraper
 
+API_PORT = 6768
 
-if __name__ == "__main__":
+
+def start_api(app: FastAPI):
+    uvicorn.run(app, host="0.0.0.0", port=API_PORT)
+
+
+def start_scraper():
     s = Scraper()
     s.start()
 
+
+
+Thread(target=start_api, args=(app,)).start()
+start_scraper()  # needs to run in main Thread so it can handle SIGINT/SIGTERM
