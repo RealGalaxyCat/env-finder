0ebaf4f89ce2ebdc3041b62e9d092c63ebfc6e5b
realgalaxycat <111510472+RealGalaxyCat@users.noreply.github.com>
2026-05-16 18:26:12 +0200

    logs: add custom logger class with getter, and 'SECRET_FOUND' log level

--- env_finder/logger.py
+++ env_finder/logger.py
@@ -1,17 +1,34 @@
 import sys
-from colorama import Fore, Style
+from colorama import Fore, Style, Back
 import logging
 from logging import StreamHandler
 from logging.handlers import RotatingFileHandler
 
 
+SECRET_FOUND = 25  # between INFO (20) and WARNING (30)
+logging.addLevelName(SECRET_FOUND, "SECRET_FOUND")
+
+
 COLORS = {
     logging.DEBUG:    Fore.CYAN,
     logging.INFO:     Fore.GREEN,
+    SECRET_FOUND:     Back.YELLOW + Fore.WHITE,
     logging.WARNING:  Fore.YELLOW,
     logging.ERROR:    Fore.RED,
     logging.CRITICAL: Fore.MAGENTA,
 }
+
+
+class AppLogger(logging.Logger):
+    def secret(self, message, *args, **kwargs):
+        if self.isEnabledFor(SECRET_FOUND):
+            self._log(SECRET_FOUND, message, args, **kwargs)
+
+
+# use CustomLogger for all new loggers
+logging.setLoggerClass(AppLogger)
+
+
 
 class ColorFormatter(logging.Formatter):
     def format(self, record):
@@ -35,7 +52,7 @@ def setup_logger(root_level, filename: str):
     fh = RotatingFileHandler(
         f"/app/logs/{filename}",
         mode="a",
-        maxBytes=5 * 1024 * 1024,
+        maxBytes=5 * 1024 * 1024,  # 5mb
         backupCount=3
     )
     fh.setFormatter(logging.Formatter(fmt))
@@ -43,3 +60,9 @@ def setup_logger(root_level, filename: str):
 
     root.addHandler(sh)
     root.addHandler(fh)
+
+
+# getter, only exists for type hinting
+def getLogger(name: str) -> AppLogger:
+    return logging.getLogger(name)
+
