eadd08cf7ab406eb7a682f9d4f5eb5e3153a51fb
realgalaxycat <111510472+RealGalaxyCat@users.noreply.github.com>
2026-05-16 18:00:49 +0200

    logs: add colored formatting for console handler

--- env_finder/logger.py
+++ env_finder/logger.py
@@ -1,7 +1,24 @@
 import sys
+from colorama import Fore, Style
 import logging
 from logging import StreamHandler
 from logging.handlers import RotatingFileHandler
+
+
+COLORS = {
+    logging.DEBUG:    Fore.CYAN,
+    logging.INFO:     Fore.GREEN,
+    logging.WARNING:  Fore.YELLOW,
+    logging.ERROR:    Fore.RED,
+    logging.CRITICAL: Fore.MAGENTA,
+}
+
+class ColorFormatter(logging.Formatter):
+    def format(self, record):
+        color = COLORS.get(record.levelno, "")
+        record.levelname = f"{color}{record.levelname}{Style.RESET_ALL}"
+        return super().format(record)
+
 
 
 
@@ -9,18 +26,20 @@ def setup_logger(root_level, filename: str):
     root = logging.getLogger()
     root.setLevel(root_level)
 
-    formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+    fmt = "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
 
+    # handlers
     sh = StreamHandler(sys.stdout)
+    sh.setFormatter(ColorFormatter(fmt))
+
     fh = RotatingFileHandler(
         f"/app/logs/{filename}",
         mode="a",
         maxBytes=5 * 1024 * 1024,
         backupCount=3
     )
+    fh.setFormatter(logging.Formatter(fmt))
 
-    sh.setFormatter(formatter)
-    fh.setFormatter(formatter)
 
     root.addHandler(sh)
     root.addHandler(fh)
