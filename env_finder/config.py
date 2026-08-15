3da0992b6309d99df96de3c582873ffc57228858
realgalaxycat <111510472+RealGalaxyCat@users.noreply.github.com>
2026-08-15 10:04:18 +0200

    feat: introduce custom error delay

--- env_finder/config.py
+++ env_finder/config.py
@@ -5,13 +5,16 @@ from pydantic import Field, field_validator
 
 
 class Config(BaseSettings):
-    github_pat: str = Field(description="Github Personal Access Token, required for scraping")
+    github_pat: str          = Field(description="Github Personal Access Token, required for scraping")
+    error_delay: float       = Field(default=5.0, description="Sleep delay after encountering an error during scraping")
+
 
     # For displaying a custom error message
     @field_validator("github_pat", mode="before")
     def pat_is_set(cls, v: str):
         if not v:
             raise ValueError("A Github PAT is required for this program to work. Add it to .env as 'GITHUB_PAT'")
+
 
 
 @lru_cache
